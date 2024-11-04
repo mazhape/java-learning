cd4a9ba8656965db84a6bc836802afd6e387f4e1
Tatenda <mazhape98@gmail.com>
2024-11-04 10:25:31 +0200

    StringBufferExample

--- StringBufferExample.java
+++ StringBufferExample.java
@@ -1,6 +1,7 @@
 public class StringBufferExample {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer();
+        // StringBuffer sb = new StringBuffer("durga");
         System.out.println(sb.capacity());// 16
         sb.append("abcdefghijklmnop");
         System.out.println(sb.capacity());// 16
